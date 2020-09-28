9dd9caf98675e8eae68f6b21cd66e18214c85073
Dec <2312225342@qq.com>
2020-09-28 23:41:26 +0800

    menu init

--- atcrowdfunding01-admin-parent/atcrowdfunding02-admin-webui/src/test/java/com/atguigu/crowd/test/StringTest.java
+++ atcrowdfunding01-admin-parent/atcrowdfunding02-admin-webui/src/test/java/com/atguigu/crowd/test/StringTest.java
@@ -7,9 +7,8 @@ import org.junit.Test;
 public class StringTest {
     @Test
     public void testMD5() {
-        String source = "1234";
+        String source = "123456";
         String s = CrowdUtil.md5(source);
         System.out.println(s);
-
     }
 }
